f31b22f283a6e80e67124242c846286e595de17c
bipblop555 <tom.bonnet@hotmail.com>
2023-09-12 15:32:27 +0200

    reformat test

--- gildedRose.test.ts
+++ gildedRose.test.ts
@@ -1,13 +1,14 @@
-import {Item, GildedRose} from "./src/index"
+import { Item, GildedRose } from './src/index'; // Assurez-vous de mettre le chemin correct vers votre code source
 
 describe('GildedRose', () => {
-    // Test pour la mise à jour de la qualité de l'Item "Aged Brie"
-    it('should update Aged Brie quality correctly', () => {
-      const agedBrie = new Item('Aged Brie', 10, 20);
-      const gildedRose = new GildedRose([agedBrie]);
-  
-      gildedRose.updateQuality();
-  
-      expect(agedBrie.quality).toBe(21);
-    });
-})
+  // Test pour la mise à jour de la qualité de l'Item "Aged Brie"
+  it('should update Aged Brie quality correctly', () => {
+    const agedBrie = new Item('Aged Brie', 10, 20);
+    const gildedRose = new GildedRose([agedBrie]);
+
+    gildedRose.updateQuality();
+
+    expect(agedBrie.quality).toBe(21);
+  });
+
+});
