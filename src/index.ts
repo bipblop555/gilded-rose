d13a9f6b258191d87fdfc5d34dbc2181fdaf8895
bipblop555 <tom.bonnet@hotmail.com>
2023-09-12 15:12:37 +0200

    refacto plusieurs fcts

--- src/index.ts
+++ src/index.ts
@@ -17,44 +17,91 @@ export class GildedRose {
       this.items = items;
   }
 
-  updateQuality(): Array<Item> {
-      this.items.forEach(item => {
-          switch (item.name) {
-              case "Aged Brie":
-                  this.increaseQuality(item);
-                  break;
-              case "Backstage passes to a TAFKAL80ETC concert":
-                  this.increaseQuality(item);
-                  if (item.sellIn < 11) this.increaseQuality(item);
-                  if (item.sellIn < 6) this.increaseQuality(item);
-                  if (item.sellIn < 0) item.quality = 0;
-                  break;
-              case "Sulfuras, Hand of Ragnaros":
-                  break;
-              default:
-                  this.decreaseQuality(item);
-          }
-
-          if (item.name !== "Sulfuras, Hand of Ragnaros") {
-              item.sellIn--;
-              if (item.sellIn < 0) {
-                  item.name === "Aged Brie" ? this.increaseQuality(item) : this.decreaseQuality(item);
-              }
-          }
-      });
-
-      return this.items;
+  private isAgedBrie(item: Item) {
+    if(item.name === 'Aged Brie'){
+      console.log("true");
+      return true
+    }
+    else return false;
   }
 
-  increaseQuality(item: Item): void {
+  private isBackstagePass(item: Item) {
+    if(item.name === 'Backstage passes to a TAFKAL80ETC concert'){
+      console.log("true");
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  private isSulfuras(item: Item) {
+    if(item.name === 'Sulfuras, Hand of Ragnaros'){
+      console.log("true")
+      return true
+    }
+    else{
+      return false
+    }  
+}
+
+  private isConjured(item: Item) {
+    if( item.name.includes('Conjured'))
+    {
+      console.log("true");
+      return true
+    }
+    return false;
+  }
+
+  private updateAgedBrieQuality(item: Item) {
+    if (item.quality < 50) {
+      console.log("trueeee");
+      item.quality++;
+    }
+  }
+
+  private updateBackstagePassQuality(item: Item) {
     if (item.quality < 50) {
       item.quality++;
-  }
+      if (item.sellIn <= 10) {
+        item.quality++;
+      }
+      if (item.sellIn <= 5) {
+        item.quality++;
+      }
+    }
+    if (item.sellIn <= 0) {
+      item.quality = 0;
+    }
   }
 
-  decreaseQuality(item: Item): void {
+  private updateNormalItemQuality(item: Item) {
     if (item.quality > 0) {
-      item.quality--;
+      const qualityChange = this.isConjured(item) ? 2 : 1;
+      item.quality -= qualityChange;
+    }
   }
+  
+  private updateQualityForItem(item: Item) {
+    if (!this.isSulfuras(item)) {
+      item.sellIn--;
+
+      if (this.isAgedBrie(item)) {
+        this.updateAgedBrieQuality(item);
+      } else if (this.isBackstagePass(item)) {
+        this.updateBackstagePassQuality(item);
+      } else {
+        this.updateNormalItemQuality(item);
+      }
+    }
+  }
+
+  updateQuality() {
+    for (const item of this.items) {
+      this.updateQualityForItem(item);
+    }
+
+    return this.items;
   }
 }
