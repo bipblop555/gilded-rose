fee7410126d067c7f37a015669d2eee614c81e10
bipblop555 <tom.bonnet@hotmail.com>
2023-09-12 15:35:17 +0200

    modifs

--- gildedRose.test.ts
+++ gildedRose.test.ts
@@ -2,7 +2,7 @@ import { Item, GildedRose } from './src/index'; // Assurez-vous de mettre le che
 
 describe('GildedRose', () => {
   // Test pour la mise à jour de la qualité de l'Item "Aged Brie"
-  it('should update Aged Brie quality correctly', () => {
+  it('Devrait update AgedBrie correctement', () => {
     const agedBrie = new Item('Aged Brie', 10, 20);
     const gildedRose = new GildedRose([agedBrie]);
 
@@ -11,7 +11,7 @@ describe('GildedRose', () => {
     expect(agedBrie.quality).toBe(21);
   });
 
-  it('should update Backstage Pass quality correctly', () => {
+  it('Devrait update la qualité de Backstage Pass', () => {
     const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20);
     const gildedRose = new GildedRose([backstagePass]);
 
